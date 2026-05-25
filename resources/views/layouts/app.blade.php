e5887620294d3adb445cc0832a6e4f6b94b53928
imranhussainphp <prinsekhan007@gmail.com>
2026-05-25 23:47:12 +0500

    playwrite updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow, noarchive">
     <title>Nexon Live Chat</title>
     <link rel="icon" type="image/webp" href="https://images.nexonpackaging.com/logo.webp">
     <link rel="stylesheet" href="/css/app.css">
